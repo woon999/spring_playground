4ed624afad3d58ae5a91fa9ec860f89c70ce7dba
loosie <54282927+loosie@users.noreply.github.com>
2020-02-28 00:32:26 +0900

    test

--- foody-api/src/test/java/kr/co/loosie/foody/domain/RestaurantTests.java
+++ foody-api/src/test/java/kr/co/loosie/foody/domain/RestaurantTests.java
@@ -5,12 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RestaurantTests {
 
     @Test
     public void creation(){
-        Restaurant restaurant = new Restaurant();
+        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void info(){
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+
+        assertThat(restaurant.getInfo(), is("Bob zip in Seoul"));
     }
 
 }
