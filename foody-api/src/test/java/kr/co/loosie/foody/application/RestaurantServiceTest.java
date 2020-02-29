c760bf971217a688b567c947b28d6b51a9a31d77
loosie <54282927+loosie@users.noreply.github.com>
2020-02-29 21:09:48 +0900

    Mock활용(가짜객체)

--- foody-api/src/test/java/kr/co/loosie/foody/application/RestaurantServiceTest.java
+++ foody-api/src/test/java/kr/co/loosie/foody/application/RestaurantServiceTest.java
@@ -1,33 +1,62 @@
 package kr.co.loosie.foody.application;
 
-import jdk.internal.jimage.ImageStream;
-import kr.co.loosie.foody.domain.*;
+import kr.co.loosie.foody.domain.MenuItem;
+import kr.co.loosie.foody.domain.MenuItemRepository;
+import kr.co.loosie.foody.domain.Restaurant;
+import kr.co.loosie.foody.domain.RestaurantRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
 
 class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
+    @Mock
     private RestaurantRepository restaurantRepository;
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     @BeforeEach
     public void setUp(){
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
+//      Mock이 붙인 객체들을 초기화
+        MockitoAnnotations.initMocks(this);
 
-        restaurantService = new RestaurantService(restaurantRepository,menuItemRepository);
+        mockRestaurantRepository();
+        mockMenuItemRepository();
+
+        restaurantService = new RestaurantService(
+                restaurantRepository,menuItemRepository);
 
     }
 
+
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+        restaurants.add(restaurant);
+
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+    }
+
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
+    }
     @Test
     public void getRestaurants(){
         List<Restaurant> restaurants = restaurantService.getRestaurants();
+
 
         Restaurant restaurant = restaurants.get(0);
         assertThat(restaurant.getId(), is(1004L));
--- foody-api/src/test/java/kr/co/loosie/foody/interfaces/RestaurantControllerTest.java
+++ foody-api/src/test/java/kr/co/loosie/foody/interfaces/RestaurantControllerTest.java
@@ -1,19 +1,21 @@
 package kr.co.loosie.foody.interfaces;
 
 import kr.co.loosie.foody.application.RestaurantService;
-import kr.co.loosie.foody.domain.MenuItemRepository;
-import kr.co.loosie.foody.domain.MenuItemRepositoryImpl;
-import kr.co.loosie.foody.domain.RestaurantRepository;
-import kr.co.loosie.foody.domain.RestaurantRepositoryImpl;
+import kr.co.loosie.foody.domain.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,20 +28,27 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private RestaurantService restaurantService;
+
     //  controller가 repository에 직접적으로 의존하고 있었는데
 //  이렇게 Impl과 inteface로 나누어 의존성을 분리를 통해 의존성주입의 장점을 살림
 //    (RestaurantRepositoryImpl.class) 우리가 사용하는 객체를 다양하게 변경가능
-    @SpyBean(RestaurantRepositoryImpl.class)
+ /*   @SpyBean(RestaurantRepositoryImpl.class)
     private RestaurantRepository restaurantRepository;
 
     @SpyBean(MenuItemRepositoryImpl.class)
     private MenuItemRepository menuItemRepository;
+*/
 
-    @SpyBean(RestaurantService.class)
-    private RestaurantService restaurantService;
+
 
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants= new ArrayList<>();
+        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
@@ -51,6 +60,12 @@ public class RestaurantControllerTest {
 
     @Test
     public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L,"Bob zip","Seoul");
+        Restaurant restaurant2 = new Restaurant(2020L,"Cyber Food","Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
