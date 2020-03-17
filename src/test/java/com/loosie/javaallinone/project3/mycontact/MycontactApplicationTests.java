08a4b34c4f421d3d55c261c17390c3f5ef0a266d
loosie <54282927+loosie@users.noreply.github.com>
2020-03-17 19:38:29 +0900

    Initial commit

---
+++ src/test/java/com/loosie/javaallinone/project3/mycontact/MycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loosie.javaallinone.project3.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
