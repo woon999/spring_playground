3030c2ef4efe47dce44c23f20e4cf262395a5632
loosie <54282927+loosie@users.noreply.github.com>
2020-03-19 10:17:38 +0900

    lombok @Data,@EqualsAndHashCode,@Constructor

--- src/main/java/com/loosie/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/loosie/javaallinone/project3/mycontact/domain/Person.java
@@ -9,12 +9,10 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class Person {
 
     @Id
@@ -38,22 +36,8 @@ public class Person {
     @ToString.Exclude
     private String phoneNumber;
 
-    public boolean equals(Object object){
-        if(object == null){
-            return false;
-        }
 
-        Person person = (Person)object;
 
-        if(!person.getName().equals(this.getName())){
-            return false;
-        }
 
-        if(person.getAge() != this.getAge()){
-            return false;
-        }
-
-        return true;
-    }
 
 }
--- src/test/java/com/loosie/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/loosie/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,5 +47,11 @@ class PersonRepositoryTest {
         System.out.println(person1.equals(person2));
         System.out.println(person1.hashCode());
         System.out.println(person2.hashCode());
+
+        Map<Person,Integer> map = new HashMap<>();
+        map.put(person1,person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
     }
 }
