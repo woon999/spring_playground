972a702ef8727e0cd1dd4db67b2e2250f19218d8
loosie <54282927+loosie@users.noreply.github.com>
2020-03-27 14:40:50 +0900

    Service Test#1 - @SpringBootTest -> @ExtendWith(MockitoExtension.class)

--- src/test/java/com/loosie/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/loosie/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -1,24 +1,35 @@
 package com.loosie.javaallinone.project3.mycontact.service;
 
 import com.loosie.javaallinone.project3.mycontact.domain.Person;
+import com.loosie.javaallinone.project3.mycontact.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
-    @Autowired
+    @InjectMocks
     private PersonService personService;
-
+    @Mock
+    private PersonRepository personRepository;
 
     @Test
     void getPeopleByName(){
+        when(personRepository.findByName("martin"))
+                .thenReturn(Lists.newArrayList(new Person("martin")));
+
         List<Person> result = personService.getPeopleByName("martin");
 
         assertThat(result.size()).isEqualTo(1);
@@ -26,11 +37,5 @@ class PersonServiceTest {
     }
 
 
-    @Test
-    void getPerson(){
-        Person person = personService.getPerson(3L);
-
-        assertThat(person.getName()).isEqualTo("dennis");
-    }
 
 }
