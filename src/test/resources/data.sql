adacaf7e8fb679a8564d302d886fcff77bd80d2b
loosie <54282927+loosie@users.noreply.github.com>
2020-03-27 14:30:28 +0900

    Repository Test

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,3 +3,6 @@ insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_o
 insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (3, 'dennis',1993,10,15);
 insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (4, 'sophia',1994,8,31);
 insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`) values (5, 'benny',1995,12,23);
+insert into person(`id`, `name`, `year_of_birthday`, `month_of_birthday`, `day_of_birthday`, `job`, `hobby`, `phone_number`, `address`)
+    values (6, 'tony', 1991, 7, 10, 'officer', 'reading', '010-2222-5555', 'seoul');
+insert into person(`id`, `name`, `deleted`) values (7, 'andrew', true);
