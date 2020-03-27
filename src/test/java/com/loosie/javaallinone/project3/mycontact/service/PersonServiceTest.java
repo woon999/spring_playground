34f786bb35663360f2c9d9e4ebd0ebd6edba171e
loosie <54282927+loosie@users.noreply.github.com>
2020-03-27 15:12:09 +0900

    Service Test#3 - ArgumentMatcher<Person>

--- src/test/java/com/loosie/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/loosie/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -2,10 +2,12 @@ package com.loosie.javaallinone.project3.mycontact.service;
 
 import com.loosie.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.loosie.javaallinone.project3.mycontact.domain.Person;
+import com.loosie.javaallinone.project3.mycontact.domain.dto.Birthday;
 import com.loosie.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
@@ -60,12 +63,56 @@ class PersonServiceTest {
 
     @Test
     void put(){
-        PersonDto dto = PersonDto.of("martin","programming"
-                , "판교", LocalDate.now(),"programmer"
-                , "010-1111-2222");
-
-        personService.put(dto);
+        personService.put(mockPersonDto());
 
         verify(personRepository, times(1)).save(any(Person.class));
+    }
+
+
+    @Test
+    void modifyIfPersonNotFound(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, ()-> personService.modify(1L, mockPersonDto()));
+    }
+
+    @Test
+    void modifyIfNameIsDifferent(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("tony")));
+
+        assertThrows(RuntimeException.class, ()-> personService.modify(1L,mockPersonDto()));
+    }
+
+    @Test
+    void modify(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        personService.modify(1L,mockPersonDto());
+
+//        verify(personRepository,times(1)).save(any(Person.class));
+        verify(personRepository, times(1)).save(argThat(new IsPersonWillBeUpdated()));
+    }
+
+    private PersonDto mockPersonDto(){
+        return PersonDto.of("martin","programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+    }
+
+    private static class IsPersonWillBeUpdated implements ArgumentMatcher<Person>{
+        @Override
+        public boolean matches(Person person) {
+            return equals(person.getName(), "martin")
+                    && equals(person.getHobby(), "programming")
+                    && equals(person.getAddress(), "판교")
+                    && equals(person.getBirthday(), Birthday.of(LocalDate.now()))
+                    && equals(person.getJob(), "programmer")
+                    && equals(person.getPhoneNumber(), "010-1111-2222");
+        }
+
+        private boolean equals(Object actual, Object expected) {
+            return expected.equals(actual);
+        }
     }
 }
