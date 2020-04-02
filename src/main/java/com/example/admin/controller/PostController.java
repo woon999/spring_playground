993013a3344b6998954b780b0eebd133b1412692
loosie <54282927+loosie@users.noreply.github.com>
2020-04-02 14:41:58 +0900

    HTTP - PUT Method

---
+++ src/main/java/com/example/admin/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.example.admin.controller;
+
+
+import com.example.admin.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    // HTML <Form>
+    // ajax 검색
+    // http post body -> data
+    // json, xml, multipart-form / text-plain
+
+    @PostMapping(value = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+
+        return searchParam;
+    }
+
+
+}
