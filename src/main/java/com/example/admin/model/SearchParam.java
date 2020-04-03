57b1bbe11537cf013dc42151b677271625d6cfbe
loosie <54282927+loosie@users.noreply.github.com>
2020-04-03 16:01:47 +0900

    Lombok

--- src/main/java/com/example/admin/model/SearchParam.java
+++ src/main/java/com/example/admin/model/SearchParam.java
@@ -1,32 +1,15 @@
 package com.example.admin.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SearchParam {
 
     private String account;
     private String email;
     private int page;
 
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
