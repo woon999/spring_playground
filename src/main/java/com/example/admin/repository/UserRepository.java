8f4802e41a496ca3bcfd1e74580272a48b5c4090
loosie <54282927+loosie@users.noreply.github.com>
2020-04-07 15:17:47 +0900

    JPA -QueryMethod

--- src/main/java/com/example/admin/repository/UserRepository.java
+++ src/main/java/com/example/admin/repository/UserRepository.java
@@ -5,7 +5,17 @@ import com.example.admin.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    // select * from user where account =? << test03, test04
+    Optional<User> findByAccount(String account);
+
+    Optional<User> findByEmail(String email);
+
+    // select * from user where account = ? and email = ?
+    Optional<User> findByAccountAndEmail(String account, String email);
 
 }
--- src/test/java/com/example/admin/repository/UserRepositoryTest.java
+++ src/test/java/com/example/admin/repository/UserRepositoryTest.java
@@ -35,7 +35,9 @@ public class UserRepositoryTest extends AdminApplicationTests {
     @Test
     @Transactional
     public void read() {
-        Optional<User> user = userRepository.findById(1L);
+
+        //select * from user where id = ?
+        Optional<User> user = userRepository.findByAccount("TestUser03");
 
         user.ifPresent(selectUser -> {
             selectUser.getOrderDetailList().stream().forEach(detail->{
