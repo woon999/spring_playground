2cddb483cc5606fc5c1f181c05f9fc48657acdc3
loosie <54282927+loosie@users.noreply.github.com>
2020-04-08 15:36:28 +0900

    Repository생성

--- src/main/java/com/example/admin/repository/CategoryRepository.java
+++ src/main/java/com/example/admin/repository/CategoryRepository.java
@@ -1,14 +1,9 @@
 package com.example.admin.repository;
 
-
-import com.example.admin.model.entity.User;
+import com.example.admin.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
--- src/main/java/com/example/admin/repository/OrderGroupRepository.java
+++ src/main/java/com/example/admin/repository/OrderGroupRepository.java
@@ -1,14 +1,9 @@
 package com.example.admin.repository;
 
-
-import com.example.admin.model.entity.User;
+import com.example.admin.model.entity.OrderGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
+public interface OrderGroupRepository extends JpaRepository<OrderGroup,Long> {
 }
