c884d4e97358c5af6445edf64dc7d0b6cbe969e5
loosie <54282927+loosie@users.noreply.github.com>
2020-04-15 18:28:57 +0900

    사용자 API 삭제

--- src/main/java/com/example/admin/controller/api/UserApiController.java
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -34,13 +34,12 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
     @Override
     @PutMapping("") // /api/user
     public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
-
         return userApiLogicService.update(request);
     }
 
     @Override
     @DeleteMapping("{id}") // /api/user/{id}
     public Header delete(@PathVariable Long id) {
-        return null;
+        return userApiLogicService.delete(id);
     }
 }
--- src/main/java/com/example/admin/service/UserApiLogicService.java
+++ src/main/java/com/example/admin/service/UserApiLogicService.java
@@ -81,7 +81,16 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
 
     @Override
     public Header delete(Long id) {
-        return null;
+        // 1. id -> repository -> user
+        Optional<User> optional = userRepository.findById(id);
+
+        // 2. repository -> delete
+        return optional.map(user -> {
+            userRepository.delete(user);
+            return Header.OK();
+        })
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
+
     }
 
     private Header<UserApiResponse> response(User user) {
