9686aaa22cf9d3c03b7e91c2c1aa9075b566c185
loosie <54282927+loosie@users.noreply.github.com>
2020-04-26 15:00:57 +0900

    user,item,ordergroup controller 리팩토링(추상화)

--- src/main/java/com/example/admin/controller/api/UserApiController.java
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -1,14 +1,13 @@
 package com.example.admin.controller.api;
 
 import com.example.admin.controller.CrudController;
-import com.example.admin.ifs.CrudInterface;
-import com.example.admin.model.network.Header;
 import com.example.admin.model.network.request.UserApiRequest;
 import com.example.admin.model.network.response.UserApiResponse;
 import com.example.admin.service.UserApiLogicService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
