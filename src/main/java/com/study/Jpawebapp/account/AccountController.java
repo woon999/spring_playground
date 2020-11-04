fbf1423165272310274e6975d779f690ee3bdc8f
jongwon <54282927+loosie@users.noreply.github.com>
2020-11-05 02:51:24 +0900

    #5. SignUpForm Submit 검증

--- src/main/java/com/study/Jpawebapp/account/AccountController.java
+++ src/main/java/com/study/Jpawebapp/account/AccountController.java
@@ -1,11 +1,34 @@
 package com.study.Jpawebapp.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+
+    /**
+     * SignUpForm데이터를 받을때 바인더를 설정
+     * public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors)
+     * @Valid SignUpForm이랑 매핑
+     */
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
@@ -15,5 +38,16 @@ public class AccountController {
         return "account/sign-up";
     }
 
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+
+        // TODO : 회원가입 처리
+        return "redirect:/";
+
+    }
 
 }
