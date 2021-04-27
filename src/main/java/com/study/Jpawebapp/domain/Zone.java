baa554848977afdb9e944c0a6f2a83d3549f705e
jongwon <54282927+loosie@users.noreply.github.com>
2021-04-27 12:13:02 +0900

    #40 지역 도메인

---
+++ src/main/java/com/study/Jpawebapp/domain/Zone.java
@@ -0,0 +1,26 @@
+package com.study.Jpawebapp.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Zone {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String localNameOfCity;
+
+    @Column(nullable = false)
+    private String province;
+}
---
+++ src/main/java/com/study/Jpawebapp/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.study.Jpawebapp.zone;
+
+import com.study.Jpawebapp.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
