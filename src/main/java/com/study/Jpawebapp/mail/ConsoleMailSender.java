9e26c75de5bbbb02cf9708d8275ab0aa44bc2a66
jongwon <54282927+loosie@users.noreply.github.com>
2021-04-28 12:17:17 +0900

    #42 PostgreSQL 설치 및 설정

--- src/main/java/com/study/Jpawebapp/mail/ConsoleMailSender.java
+++ src/main/java/com/study/Jpawebapp/mail/ConsoleMailSender.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.mail.internet.MimeMessage;
 import java.io.InputStream;
 
-@Profile("local")
+@Profile({"local", "dev"})
 @Component
 @Slf4j
 public class ConsoleMailSender implements JavaMailSender {
