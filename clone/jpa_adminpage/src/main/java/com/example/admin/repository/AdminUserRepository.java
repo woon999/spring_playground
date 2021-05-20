c88bb06cbee474d341fa3d63a043fa382cf499a7
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-20 22:23:24 +0900

    ##

--- clone/jpa_adminpage/src/main/java/com/example/admin/repository/AdminUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.repository;
-
-import com.example.admin.model.entity.AdminUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminUserRepository extends JpaRepository<AdminUser,Long> {
-}
--- clone/jpa_adminpage/src/main/java/com/example/admin/repository/OrderGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.repository;
-
-import com.example.admin.model.entity.OrderGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderGroupRepository extends JpaRepository<OrderGroup,Long> {
-}
