6b8a1b99c77442fbd6639d3850f753f0e63f47fa
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-23 14:55:16 +0900

    #16 값 타입 - 임베디드 타입

---
+++ jpa_theory/src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,20 @@
+package hellojpa;
+
+public class ValueMain {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = a;
+//
+//        b = 20;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+
+        Integer a = new Integer(10);
+        Integer b = a;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
