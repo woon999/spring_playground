be150efca0efd0f86c33ae9ace38a7cfcb849413
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-29 12:39:19 +0900

    # jpql case, 함수

--- jpa_theory/src/main/java/hellojpa/JpaMain.java
+++ jpa_theory/src/main/java/hellojpa/JpaMain.java
@@ -524,18 +524,42 @@ public class JpaMain {
             member.setAge(10);
             member.setTeam(team);
             em.persist(member);
-
-            em.flush();
-            em.clear();
+//
+//            em.flush();
+//            em.clear();
 //            String query = "select m from Member m, Team t where m.useranme = t.name";
 //            List<Member> resultList = em.createQuery(query, Member.class)
 //                    .getResultList();
 
             // 서브 쿼리 (FROM x)
-            String query = "select mm.age, mm.username " +
-                    "from from (select m.age, m.useranme from Member m) as mm";
-            List<Member> resultList = em.createQuery(query, Member.class)
+//            String query = "select mm.age, mm.username " +
+//                    "from from (select m.age, m.useranme from Member m) as mm";
+//            List<Member> resultList = em.createQuery(query, Member.class)
+//                    .getResultList();
+
+            // CASE
+//            String query = "select " +
+//                            "case when m.age <= 10 then '학생요금' " +
+//                            "     when m.age >= 60 then '경로요금' " +
+//                            "     else '일반요금' " +
+//                            "end " +
+//                    "from Member m";
+//            List<String> resultList = em.createQuery(query, String.class)
+//                    .getResultList();
+//            for(String s : resultList){
+//                System.out.println("s = " + s);
+//            }
+
+
+            // jpql 함수
+            String query = "select concat('a', 'b') From Member m  ";
+            List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
+            for(String s : resultList){
+                System.out.println("s = " + s);
+            }
+
+
 
             // 트랜잭션 커밋
             tx.commit();
