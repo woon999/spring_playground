fcc71a1ddf247f763aa2fa32388b8ced5fbdcdfa
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-18 22:07:24 +0900

    #4-1 게시글 등록 뷰 생성

--- clone/freelec-springboot2-webservice/src/main/java/com/loosie/book/springboot/web/IndexController.java
+++ clone/freelec-springboot2-webservice/src/main/java/com/loosie/book/springboot/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
