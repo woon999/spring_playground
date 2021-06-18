09632354c7bc734507b95e169b8254511c03f7a4
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-19 00:14:39 +0900

    #4-3 게시글 수정 뷰 생성 및 테스트

--- clone/freelec-springboot2-webservice/src/main/java/com/loosie/book/springboot/web/IndexController.java
+++ clone/freelec-springboot2-webservice/src/main/java/com/loosie/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.loosie.book.springboot.web;
 
 import com.loosie.book.springboot.service.PostsService;
+import com.loosie.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
