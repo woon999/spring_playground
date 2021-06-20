3e93f71306a79c725d9cc2bbffeb20aeb147aa0e
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-21 01:25:05 +0900

    #5-5 Oauth2 네이버 로그인 연동

--- clone/freelec-springboot2-webservice/src/main/java/com/loosie/book/springboot/config/auth/dto/OAuthAttributes.java
+++ clone/freelec-springboot2-webservice/src/main/java/com/loosie/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,7 +25,23 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
