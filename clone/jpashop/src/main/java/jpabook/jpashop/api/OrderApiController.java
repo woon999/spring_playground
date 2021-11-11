43861389aacbd996ad90ec45fa86398d4d7e26b8
jongwon <54282927+loosie@users.noreply.github.com>
2021-11-11 11:56:24 +0900

    #jpashop - 주문 조회 V1: 엔티티 직접 노출

--- clone/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ clone/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,4 +1,32 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    // hibernate5Module을 이용해 Lazy 프록시 객체 강제 초기화
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
 }
