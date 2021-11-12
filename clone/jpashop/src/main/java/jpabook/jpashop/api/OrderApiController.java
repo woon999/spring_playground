ea7c1021a1be63a4d707679add272642f38489e0
jongwon <54282927+loosie@users.noreply.github.com>
2021-11-12 14:55:03 +0900

    #jpashop - 주문 조회 V3 : 엔티티를 DTO로 변환 - 페치 조인 최적화(페이징 불가능)

--- clone/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ clone/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -56,6 +56,15 @@ public class OrderApiController {
         return collect;
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
     @Data
     static class OrderDto{
         private Long orderId;
--- clone/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ clone/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -102,6 +102,16 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d"+
+                        " join fetch  o.orderItems oi"+
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
 
 
