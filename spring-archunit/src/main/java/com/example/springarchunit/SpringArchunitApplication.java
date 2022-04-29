4a55e24e3ad818027310a28619d1e0d6bbc9b7d8
jongwon <54282927+loosie@users.noreply.github.com>
2022-04-29 21:08:22 +0900

    feat: spring-archunit 아키텍처 테스트 프로젝트  생성

---
+++ spring-archunit/src/main/java/com/example/springarchunit/SpringArchunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springarchunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArchunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArchunitApplication.class, args);
+	}
+
+}
